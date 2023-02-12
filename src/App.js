d283329d3fae640953595af721c07cd1636d71ad
Mychal Culpepper <mychalculpepper@Mychals-MBP.lan>
2023-02-11 20:20:32 -0600

    imported react-router-dom and created /Neighborhoods route (works)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import Nav from './Components/Nav';
 import SignupForm from "./Components/SignupForm";
+import { Route, Routes } from "react-router-dom";
+import Neighborhood from "./Components/Neighborhood";
 
 function App() {
   
   return (
     <>
-    <Nav />
-    <div className="container">
-      <SignupForm />
-    </div>
+    <Routes>
+      <Route path="/" element={<SignupForm/>} />
+      <Route path="/Neighborhood" element={<Neighborhood/>} />
+    </Routes>
     </>
 
   );
---
+++ src/Components/Neighborhood.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Neighborhood = () => {
+ 
+  return (
+    <h1>Neighborhood</h1>
+  );
+};
+
+export default Neighborhood;
